d23007ef2cce4e523c0cb6962cd928072e1ac1c1
KatarinaNN <katya.naz@gmail.com>
2023-12-18 20:54:03 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { useEffect, useState } from 'react';
+
 
 function App() {
+  const [advice, setAdvice] =useState("");
+  useEffect(()=> {
+    getAdvice()
+  }, [])
+
+  const getAdvice = async() => {
+    const response = await fetch (`http://www.boredapi.com/api/activity/`);
+    const data = await response.json();
+    console.log(data.activity);
+    setAdvice(data.activity);
+        }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>What to do....?</h1>
+      <p>{advice}</p>
+      <button onClick={getAdvice}>Fetch an idea</button>
+
     </div>
+
   );
 }
 
